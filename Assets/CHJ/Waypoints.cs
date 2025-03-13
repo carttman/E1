7486bb1b3dc62bcee555530f32c9f74751d945f0
carttman <chj990920@gmail.com>
2025-03-13 18:22:08 +0900

    몬스터 경로 이동 추가

---
+++ Assets/CHJ/Waypoints.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+           points[i] = transform.GetChild(i);
+        }
+    }
+}
