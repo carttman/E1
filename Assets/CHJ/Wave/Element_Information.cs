57f62cc82c02ba85fee4a1bfcb9e35e838e60fb3
carttman <chj990920@gmail.com>
2025-04-01 18:25:23 +0900

    속성정보 UI 수정중

--- Assets/CHJ/Wave/Element_Information.cs
+++ Assets/CHJ/Wave/Element_Information.cs
@@ -7,7 +7,9 @@ public class Element_Information : MonoBehaviour
    
     [SerializeField]private Image CurrentElementImage;
     [SerializeField]private Image NextElementImage;
-    
+
+    private Global.Element currWaveEnemyElement;
+    private Global.Element nextWaveEnemyElement;
     private void Awake()
     {
         waveSpawner = FindAnyObjectByType<WaveSpawner>();
@@ -16,19 +18,29 @@ public class Element_Information : MonoBehaviour
     private void Start()
     {
         waveSpawner.OnClickWaveStartEvent += ElementInfoChanged;
+        
         ElementInfoChanged();
     }
 
     //속성 정보 갱신
     private void ElementInfoChanged()
-    {   // 다음 웨이브까지 표시됐으면 업데이트 중지
+    {   
+        // 다음 웨이브까지 표시됐으면 업데이트 중지
         if (waveSpawner.WaveIndex == waveSpawner.Waves.Length - 1) return;
         
         // 웨이브 스포너 웨이브 정보 => 현재, 다음 웨이브 => 해당 몬스터 속성
-        var currWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex].enemy.GetComponent<StatsComponent>().element;
-        var nextWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex + 1].enemy.GetComponent<StatsComponent>().element;
+        currWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex].enemy.GetComponent<StatsComponent>().element;
+        nextWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex + 1].enemy.GetComponent<StatsComponent>().element;
+        
+        // if (waveSpawner.WaveIndex + 1 > waveSpawner.Waves.Length)
+        // {
+        //     currWaveEnemyElement = nextWaveEnemyElement;
+        //     NextElementImage.enabled = false;
+        //     
+        // }
         
         CurrentElementImage.sprite = Game.Instance.GlobalData.GetElementIcon(currWaveEnemyElement);
         NextElementImage.sprite = Game.Instance.GlobalData.GetElementIcon(nextWaveEnemyElement);
+            
     }
 }
