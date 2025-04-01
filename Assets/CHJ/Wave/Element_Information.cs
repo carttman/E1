90dc0d326a58b8555f29d0e97247321c9e48af68
carttman <chj990920@gmail.com>
2025-04-01 17:46:09 +0900

    불필요한 코드 제거

--- Assets/CHJ/Wave/Element_Information.cs
+++ Assets/CHJ/Wave/Element_Information.cs
@@ -4,13 +4,9 @@ using UnityEngine.UI;
 public class Element_Information : MonoBehaviour
 {
     private WaveSpawner waveSpawner;
-    private Wave[] waves;
-    private int waveIndex;
+   
     [SerializeField]private Image CurrentElementImage;
     [SerializeField]private Image NextElementImage;
-
-    private Global.Element currWaveEnemyElement;
-    private Global.Element nextWaveEnemyElement;
     
     private void Awake()
     {
@@ -29,8 +25,8 @@ public class Element_Information : MonoBehaviour
         if (waveSpawner.WaveIndex == waveSpawner.Waves.Length - 1) return;
         
         // 웨이브 스포너 웨이브 정보 => 현재, 다음 웨이브 => 해당 몬스터 속성
-        currWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex].enemy.GetComponent<StatsComponent>().element;
-        nextWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex + 1].enemy.GetComponent<StatsComponent>().element;
+        var currWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex].enemy.GetComponent<StatsComponent>().element;
+        var nextWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex + 1].enemy.GetComponent<StatsComponent>().element;
         
         CurrentElementImage.sprite = Game.Instance.GlobalData.GetElementIcon(currWaveEnemyElement);
         NextElementImage.sprite = Game.Instance.GlobalData.GetElementIcon(nextWaveEnemyElement);
