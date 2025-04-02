44a75bc1f763388b3ce5cad1df4c636bacd05504
carttman <chj990920@gmail.com>
2025-04-02 15:00:50 +0900

    몬스터 웨이브 ui 추가

--- Assets/CHJ/Wave/Element_Information.cs
+++ Assets/CHJ/Wave/Element_Information.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,8 @@ public class Element_Information : MonoBehaviour
    
     [SerializeField]private Image CurrentElementImage;
     [SerializeField]private Image NextElementImage;
+    [SerializeField]private TMP_Text CurrEnemyCount;
+    [SerializeField]private TMP_Text NextEnemyCount;
 
     private Global.Element currWaveEnemyElement;
     private Global.Element nextWaveEnemyElement;
@@ -30,18 +33,24 @@ public class Element_Information : MonoBehaviour
         {
             currWaveEnemyElement = nextWaveEnemyElement;
             CurrentElementImage.sprite = Game.Instance.GlobalData.GetElementIcon(currWaveEnemyElement);
-            
             NextElementImage.enabled = false;
+            
+            CurrEnemyCount.text = NextEnemyCount.text;
+            NextEnemyCount.enabled = false;
             return;
         }
 
         // 웨이브 스포너 웨이브 정보 => 현재, 다음 웨이브 => 해당 몬스터 속성
-        currWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex].enemy.GetComponent<StatsComponent>().element;
-        nextWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex + 1].enemy.GetComponent<StatsComponent>().element;
+        {
+            currWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex].enemy.GetComponent<StatsComponent>().element;
+            nextWaveEnemyElement = waveSpawner.Waves[waveSpawner.WaveIndex + 1].enemy.GetComponent<StatsComponent>().element;
+            
+            CurrentElementImage.sprite = Game.Instance.GlobalData.GetElementIcon(currWaveEnemyElement);
+            NextElementImage.sprite = Game.Instance.GlobalData.GetElementIcon(nextWaveEnemyElement);
+        }
         
-        CurrentElementImage.sprite = Game.Instance.GlobalData.GetElementIcon(currWaveEnemyElement);
-        NextElementImage.sprite = Game.Instance.GlobalData.GetElementIcon(nextWaveEnemyElement);
-        
+        CurrEnemyCount.text = waveSpawner.Waves[waveSpawner.WaveIndex].SpawnCount.ToString();
+        NextEnemyCount.text = waveSpawner.Waves[waveSpawner.WaveIndex + 1].SpawnCount.ToString();
             
     }
 }
