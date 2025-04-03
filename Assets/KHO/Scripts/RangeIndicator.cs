3acf5ab5432f22e366b3545b44589a206e33593a
singateco <singateco@gmail.com>
2025-04-03 17:02:16 +0900

    Visual Bug Fix + Balance Adjustment

--- Assets/KHO/Scripts/RangeIndicator.cs
+++ Assets/KHO/Scripts/RangeIndicator.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class RangeIndicator : MonoBehaviour
 {
     [SerializeField] [Range(0.1f, 1f)] private float yScale = 0.2f;
+    // 적 충돌이 구형이므로 더해줌
+    [SerializeField] private float plusVisualRange = 0.225f;
     private Renderer _renderer;
     private Tower _tower;
 
@@ -14,7 +16,12 @@ public class RangeIndicator : MonoBehaviour
 
     private void Start()
     {
-        //Debug.Log(_tower.TargetingRange);
-        transform.localScale = new Vector3(_tower.TargetingRange * 2, yScale, _tower.TargetingRange * 2);
+        UpdateRange();
+    }
+
+    private void UpdateRange()
+    {
+        transform.localScale = new Vector3(_tower.TargetingRange * 2 + plusVisualRange, yScale,
+            _tower.TargetingRange * 2 + plusVisualRange);
     }
 }
