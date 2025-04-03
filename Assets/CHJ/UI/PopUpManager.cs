3430de7504288b6e8b1e4b3fdf97210e8321051a
carttman <chj990920@gmail.com>
2025-04-03 11:17:17 +0900

    이름 수정

--- Assets/CHJ/UI/PopUpManager.cs
+++ Assets/CHJ/UI/PopUpManager.cs
@@ -17,11 +17,11 @@ public class PopUpManager : MonoBehaviour
         _instance = this;
     }
     
-    public void CreatePopUpUI(string amount ,Vector3 objectTcVector3, Color color, float size, string damageScale = " ")
+    public void CreatePopUpUI(string amount ,Vector3 objectVector, Color color, float size, string damageScale = " ")
     {
         Vector3 randomPos = new Vector3(Random.Range(0f, .5f), Random.Range(0f, .5f), Random.Range(0f, .5f));
         
-        GameObject popUp = Instantiate(PopUpObject, objectTcVector3 + randomPos, Quaternion.identity);
+        GameObject popUp = Instantiate(PopUpObject, objectVector + randomPos, Quaternion.identity);
         
         TextMeshProUGUI temp = popUp.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         
